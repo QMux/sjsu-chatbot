d92022ed2d6ad34e41d8d6edab3e83683684fb71
QuadM <stephen.mau@sjsu.edu>
2021-02-28 11:23:39 -0800

    Fixed the directory structure

---
+++ ReadMe.txt
@@ -0,0 +1,23 @@
+https://rasa.com/docs/rasa/installation/
+installation guide above
+
+instalation
+•	python3 -m venv ./venv
+•	source ./venv/bin/activate
+•	pip3 install rasa
+•	rasa init 	# to start it\
+
+
+Training
+•	rasa train
+
+
+run
+•	rasa run actions &
+•	rasa shell
+•	rasa shell --debug
+
+other good commands
+•	/stop       	# to stop it
+•	/restart       	# to stop it
+
---
+++ actions/actions.py
@@ -0,0 +1,77 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, EventType
+from rasa_sdk.executor import CollectingDispatcher
+import webbrowser
+
+
+class ActionVideo(Action):
+    def name(self) -> Text:
+        return "action_video"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        video_url = "https://www.youtube.com/watch?v=U6Qp-xvXsME"
+        dispatcher.utter_message("Wait... Please smile to this cute friend!")
+        webbrowser.open(video_url)
+        return []
+
+
+class ActionUserFeedbackForm(Action):
+    def name(self) -> Text:
+        return "user_feedback_form"
+
+    def run(
+            self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        required_slots = ["group",
+                          "activity_level",
+                          "barking_level",
+                          "characteristics",
+                          "coat_type",
+                          "shedding",
+                          "size",
+                          "trainability"]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                # The slot is not filled yet. Request the user to fill this slot next.
+                return [SlotSet("requested_slot", slot_name)]
+
+        # All slots are filled.
+        return [SlotSet("requested_slot", None)]
+
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_details_thanks",
+                                 Group=tracker.get_slot("group"),
+                                 Activity_level=tracker.get_slot("activity_level"),
+                                 Barking_level=tracker.get_slot("barking_level"),
+                                 Characteristics=tracker.get_slot("characteristics"),
+                                 Coat_type=tracker.get_slot("coat_type"),
+                                 Shedding=tracker.get_slot("shedding"),
+                                 Size=tracker.get_slot("size"),
+                                 Trainability=tracker.get_slot("trainability"),
+                                 )
